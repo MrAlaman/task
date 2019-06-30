d613e70debb34ddf062de09a313866229eb3c1d8
MrAlaman <mralaman17@gmail.com>
2019-07-01 00:41:57 +0300

    v1

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="container mt-4">
+
+    <table class="table table-bordered mt-4">
+      <thead class="thead-light">
+        <tr>
+          <th width="5%">#</th>
+          <th width="15%">Name</th>
+          <th width="15%">Surname</th>
+          <th width="15%">Phone</th>
+          <th width="15%">Email</th>
+          <th width="15%">Options</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.name }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" v-model="user.name">
+            </span>
+          </td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.surname }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" v-model="user.surname">
+            </span>
+          </td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.number }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm"  v-model="user.number">
+            </span>
+          </td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.email }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" type="email"  v-model="user.email">
+            </span>
+          </td>
+          <td>
+            <span v-if="editIndex !== index">
+              <button @click="edit(user, index)" class="btn btn-sm btn-outline-secondary mr-2">Edit</button>
+              <button @click="remove(user, index)" class="btn btn-sm btn-outline-secondary mr-2">Remove</button>
+            </span>
+            <span v-else>
+              <button class="btn btn-sm btn-outline-secondary mr-2" @click="cancel(user)">Cancel</button>
+              <button class="btn btn-sm btn-outline-secondary mr-2" @click="save(user)">Save</button>
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="col-3 offset-9 text-right my-3">
+      <button v-show="!editIndex" @click="add" class="btn btn-sm btn-secondary">Add user</button>
+    </div>
+    <div class="row justify-content-center align-items-center">
+      <label for="exampleFormControlTextarea1">Enter JSON</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" ref="json"></textarea>
+      <button v-show="!editIndex" @click="imp" class="btn btn-md-4 btn-secondary">Import JSON</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'MrAlaman',
+  data() {
+    return {
+      editIndex: null,
+      originalData: null,
+      users: [
+        { name: 'Cuka', surname: ' Eahal', number: "328555484", email: "lorem@ips.com" },
+        { name: 'Kubis', surname: 'Enak', number: "33664518", email: "losadrem@ips.com" },
+      ]
+    }
+  },
+
+  methods: {
+
+    add() {
+      this.originalData = null
+      this.users.push({ name: '', surname: '', number: "", email: ""})
+      this.editIndex = this.users.length - 1
+    },
+
+    edit(user, index) {
+      this.originalData = Object.assign({}, user)
+      this.editIndex = index
+    },
+
+    cancel(user) {
+      this.editIndex = null
+
+      if (!this.originalData) {
+        this.users.splice(this.users.indexOf(user), 1)
+        return
+      }
+
+      Object.assign(user, this.originalData)
+      this.originalData = null
+    },
+
+    remove(user, index) {
+      this.users.splice(index, 1)
+    },
+
+    save(user) {
+      this.originalData = null
+      this.editIndex = null
+    },
+
+    imp(user) {
+      var resp = JSON.parse(this.$refs.json.value)
+      console.log(resp.name)
+        this.users.push({ name: resp.name, surname: resp.surname, number: resp.number, email: resp.email})
+      }
+    }
+}
+</script>
+<style>
+
+</style>
