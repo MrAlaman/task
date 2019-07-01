f1b1b5400965cb761dda5788d37c499fe37f1ac6
MrAlaman <mralaman17@gmail.com>
2019-07-02 00:30:51 +0300

    v1.1

---
+++ src/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container mt-4">
+    <table class="table table-bordered mt-4">
+      <thead class="thead-light">
+        <tr>
+          <th width="5%">#</th>
+          <th width="15%">Name</th>
+          <th width="15%">Surname</th>
+          <th width="15%">Phone</th>
+          <th width="15%">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.name }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" v-model="user.name">
+            </span>
+          </td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.surname }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" v-model="user.surname">
+            </span>
+          </td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.number }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm"  v-model="user.number">
+            </span>
+          </td>
+          <td>
+            <span v-if="editIndex !== index">{{ user.email }}</span>
+            <span v-if="editIndex === index">
+              <input class="form-control form-control-sm" type="email"  v-model="user.email">
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="col-3 offset-9 text-right my-3">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'kk',
+  data() {
+    return {
+      editIndex: null,
+      originalData: null,
+      users: [
+        { name: 'Cuka', surname: ' Eahal', number: "328555484", email: "lorem@ips.com" },
+        { name: 'Kubis', surname: 'Enak', number: "33664518", email: "losadrem@ips.com" },
+      ]
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import edit from './edit.vue'
+import index from './index.vue'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+var router = new VueRouter({
+	routes: [
+		{ 
+		path: '/edit', 
+		name: 'edit',
+		component: edit 
+		},
+		{
+		path: '/',
+		name: 'main',
+		component: index
+		}
+	]
+})
+
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el: "#app",
+  render: h1 => h1(App),
+  router
+})
